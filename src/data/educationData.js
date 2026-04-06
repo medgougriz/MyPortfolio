f2a7113a805a5647478138c3764c643f1846658f
Mohamed Gougriz <mohmadgougriz@gmail.com>
2026-04-06 23:18:14 +0100

    add education data

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,12 +1,12 @@
 export const educationData = [
-    // { 
-    //     title: 'Engineering Student in Digital Development and Information Systems', 
-    //     issuer: 'Moroccan School of Engineering Sciences (EMSI) — Rabat', 
-    //     date: '2024 - 2027' 
-    // },
-    // { 
-    //     title: 'Specialized Technician in Digital Development (Full-Stack)', 
-    //     issuer: 'ISTA Hay Riad — Rabat', 
-    //     date: '2022 - 2024' 
-    // }
+    { 
+        title: 'Bachelor in Management', 
+        issuer: 'fsjes ibn zohr — Agadir', 
+        date: '2019 - 2024' 
+    },
+    { 
+        title: 'Full Stack Web Devlopment', 
+        issuer: 'Souss Massa Tech Academy — Agadir', 
+        date: '2025 - 2026' 
+    }
 ];
